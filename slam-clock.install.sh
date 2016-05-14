5d46348e68118607dbeb6fa2b5d395296bcda022
Jon Watte <slam-clock@mindcontrol.org>
2016-05-14 13:07:52 -0700

    Force i2c-bcm2708

--- slam-clock.install.sh
+++ slam-clock.install.sh
@@ -8,15 +8,20 @@ fi
 
 # make sure boot/config.txt turns on I2C
 sed -i -e '/i2c_arm/ d' /boot/config.txt
+sed -i -e '/i2c1/ d' /boot/config.txt
 echo "dtparam=i2c_arm=on" >> /boot/config.txt
+echo "dtparam=i2c1=on" >> /boot/config.txt
 
 # make sure i2c modules are loaded
 if [ -f /etc/modprobe.d/raspi-blacklist.conf ]; then
+    sed -i -e '/i2c-bcm2708/ d' /etc/modprobe.d/raspi-blacklist.conf
     sed -i -e '/i2c-dev/ d' /etc/modprobe.d/raspi-blacklist.conf
     sed -i -e '/rtc-ds1307/ d' /etc/modprobe.d/raspi-blacklist.conf
 fi
+sed -i -e '/i2c-bcm2708/ d' /etc/modules
 sed -i -e '/i2c-dev/ d' /etc/modules
 sed -i -e '/rtc-ds1307/ d' /etc/modules
+echo "i2c-bcm2708" >> /etc/modules
 echo "i2c-dev" >> /etc/modules
 echo "rtc-ds1307" >> /etc/modules
 
